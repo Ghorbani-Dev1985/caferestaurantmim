36f31a47394bfc9bc9b8e6b480fbd7709e90ff00
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-04-05 14:51:21 +0330

    feat: develop menuItemsCard component

--- src/Feature/CafeMenu/Topmenu.jsx
+++ src/Feature/CafeMenu/Topmenu.jsx
@@ -1,110 +1,75 @@
-import React from 'react'
-import {Image} from "@nextui-org/react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Pagination , Autoplay , A11y} from "swiper/modules";
-
+import React from "react";
+import { Image } from "@nextui-org/react";
+import TopMenuSlider from "../../UI/TopMenuSlider";
+import { SwiperSlide } from "swiper/react";
 
 const topMenuItems = [
-    {
-        id: 1,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-01.webp',
-        link: "cafe"
-    },
-    {
-        id: 2,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-02.webp',
-        link: "hot"
-    },
-    {
-        id: 3,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-03.webp',
-        link: "damnosh"
-    },
-    {
-        id: 4,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-04.webp',
-        link: "milkShake"
-    },
-    {
-        id: 5,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-05.webp',
-        link: "asoti"
-    },
-    {
-        id: 6,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-06.webp',
-        link: "iceCoffee"
-    },
-    {
-        id: 7,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-07.webp',
-        link: "cake"
-    },
-    {
-        id: 8,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-08.webp',
-        link: "vafel"
-    },
-    {
-        id: 9,
-        src: 'src/Assets/Images/CafeMenu/cafeMenu-09.webp',
-        link: "makatel"
-    }
-]
+  {
+    id: 1,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-01.webp",
+    link: "cafe",
+  },
+  {
+    id: 2,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-02.webp",
+    link: "hot",
+  },
+  {
+    id: 3,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-03.webp",
+    link: "damnosh",
+  },
+  {
+    id: 4,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-04.webp",
+    link: "milkShake",
+  },
+  {
+    id: 5,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-05.webp",
+    link: "asoti",
+  },
+  {
+    id: 6,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-06.webp",
+    link: "iceCoffee",
+  },
+  {
+    id: 7,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-07.webp",
+    link: "cake",
+  },
+  {
+    id: 8,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-08.webp",
+    link: "vafel",
+  },
+  {
+    id: 9,
+    src: "src/Assets/Images/CafeMenu/cafeMenu-09.webp",
+    link: "makatel",
+  },
+];
 const Topmenu = () => {
   return (
     <>
-       <div className='w-full flex-center flex-nowrap overflow-x-auto sticky top-24'>
-       <Swiper
-       loop={true}
-       autoplay={{
-        delay: 3000,
-      }}
-        slidesPerView={3}
-        breakpoints={{
-            300: {
-              slidesPerView: 3,
-            },
-            640: {
-              slidesPerView: 4,
-            },
-            768: {
-              slidesPerView: 5,
-            },
-            1024: {
-              slidesPerView: 9,
-            },
-          }}
-        centeredSlides={true}
-        grabCursor={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination , Autoplay , A11y]}
-        className="mySwiper"
-      >
-        {
-            topMenuItems.map(({id , src , link})=> (
-                    <SwiperSlide key={id}>
-                <a href={`#${link}`}>
-                <Image
-      width={512}
-      height={512}
-      alt="ghorbani-dev.ir"
-      src={src}
-      className='object-fill'
-      />
-                   </a> 
-      </SwiperSlide>
-                
-            ))
-        }
-        </Swiper>
-      </div>
+      <TopMenuSlider>
+        {topMenuItems.map(({ id, src, link }) => (
+          <SwiperSlide key={id} className="rounded-full">
+            <a href={`#${link}`}>
+              <Image
+                width={512}
+                height={512}
+                alt="ghorbani-dev.ir"
+                src={src}
+                className="object-fill"
+              />
+            </a>
+          </SwiperSlide>
+        ))}
+      </TopMenuSlider>
     </>
-  )
-}
+  );
+};
 
-export default Topmenu
+export default Topmenu;
--- src/Pages/CafeMenu/CafeMenu.jsx
+++ src/Pages/CafeMenu/CafeMenu.jsx
@@ -1,14 +1,63 @@
 import React from 'react'
 import Topmenu from '../../Feature/CafeMenu/Topmenu'
-
-
+import coffee1 from './../../Assets/Images/Coffee/coffee01.webp'
+import { Image , Divider} from "@nextui-org/react";
 
 const CafeMenu = () => {
   return (
     <>
-    <section className='container relative mt-6'>
+    <section className='container relative min-h-screen mt-6'>
         {/* Top menu */}
      <Topmenu />
+     <section>
+        <div className='flex flex-col gap-y-2 mb-8'>
+            <h2 className='font-extrabold text-2xl'>قهوه</h2>
+            <p className='flex gap-x-1'>
+                <span className='block w-28 h-1 rounded-full bg-primary'></span>
+                <span className='block size-1 rounded-full bg-primary'></span>
+                <span className='block size-1 rounded-full bg-primary'></span>
+                <span className='block size-1 rounded-full bg-primary'></span>
+            </p>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+          <div className='flex items-center gap-x-2'>
+           <div>
+           <Image
+                width={171}
+                height={171}
+                alt="ghorbani-dev.ir"
+                src={coffee1}
+                className="object-fill rounded-tl-[50px] rounded-tr-[50px] rounded-br-[150px] rounded-bl-[50px]"
+              />
+           </div>
+           <div className='flex items-center flex-1 font-extrabold '>
+              <span className='flex flex-1'>اسپرسو</span>
+              <Divider className='mx-3 shrink'/>
+              <p className='flex-center'>65,000<span>تومان</span></p>
+           </div>
+          </div>
+          <div className='flex items-center gap-x-2'>
+           <div>
+           <Image
+                width={171}
+                height={171}
+                alt="ghorbani-dev.ir"
+                src={coffee1}
+                className="object-fill rounded-tl-[50px] rounded-tr-[50px] rounded-br-[150px] rounded-bl-[50px]"
+              />
+           </div>
+           <div className='flex flex-col flex-1 font-extrabold '>
+            <div className='flex'>
+              <span className='flex flex-1 text-nowrap'>لققلق لقققلقلل  اسپرسو rrtrtt rttrttrrr rtrtyrtr</span>
+              <Divider className='mx-3 shrink'/>
+              <p className='flex-center'>65,000<span>تومان</span></p>
+            </div>
+            <p className='font-normal text-base text-gray-400'>حجم شیر بالانس</p>
+           </div>
+          </div>
+         
+        </div>
+     </section>
     </section>
       </>
   )
